dcbcf03158746aed239f8bbd3fe809cf344b9987
Mr_wu <1030095192@qq.com>
2018-03-31 10:36:41 +0000

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Mr_wu</div>
+                <div class="title">Hello Laravel! - by 吴杰伦</div>
             </div>
         </div>
     </body>
