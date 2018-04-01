a8c1f132323d1e3d5b7d013d6e21233c9013337b
Mr_wu <1030095192@qq.com>
2018-04-01 02:25:55 +0000

    Finish layout and routes

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title','Laravel') - Laravel 新手入门教程</title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+  <body>
+  	@include('layouts._header')
+
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>    
+  </body>
+</html>
